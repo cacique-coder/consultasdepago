e3516b6b00e9b99d6b8f627714ea31faceae3de1
root <root@debian>
2013-06-23 22:13:59 -0430

    fix indent

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -1,26 +1,27 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class auth extends CI_Controller {
-	private $current_user;
+  private $current_user;
 
-	function __construct() {
-		parent::__construct();
-		$this->load->model('User_model');
-		$this->load->model('Auth_model');
-		$this -> current_user = new User_model();
-	}
-	public function index(){
-		$this->load->view('login_form');
-	}
+  function __construct() {
+    parent::__construct();
+    $this->load->model('User_model');
+    $this->load->model('Auth_model');
+    $this -> current_user = new User_model();
+  }
+  public function index(){
+    $this->load->view('login_form');
+  }
 
-	public function login() {
-		$this->current_user->set_log_params($this->input->post('user'));
-		$auth = new Auth_model($this -> current_user);
-		if($auth->always_in()){
-			$this->session->set_userdata($auth->data_login_user());
-			redirect('reportes');
-		}
-		else
-			$this->load->view('login_form',array('error' => 'No se pudo iniciar sesion'));
-	}
+  public function login() {
+    $this->current_user->set_log_params($this->input->post('user'));
+    $auth = new Auth_model($this -> current_user);
+    if($auth->always_in()){
+      $this->session->sess_destroy();
+      $this->session->set_userdata($auth->data_login_user());
+      redirect('reportes');
+    }
+    else
+      $this->load->view('login_form',array('error' => 'No se pudo iniciar sesion'));
+  }
 }
--- application/views/login_form.php
+++ application/views/login_form.php
@@ -1,28 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="latin9">
-	<title> </title>
-	<?php echo link_tag('css/estilos.css'); ?>
+  <meta charset="latin9">
+  <title> </title>
+  <?php echo link_tag('css/estilos.css'); ?>
 </head>
 <body>
 
 <div id="container">
-	<div id="header">
-		<h1> Sistema de consulta de recibo de pago</h1>
-	</div>
-	<div id="body">
-		<?php if (isset($error)) : ?>	
-			<div id="error">
-				<?php echo $error;?>
-			</div>
-		<?php endif;?>
-		<?php echo form_open('auth/login'); ?>
-			<label>Usuario <input type='text' name='usuario[usuario]'/></label>
-			<label>Password <input type='password' name='usuario[password]'/></label>
-			<input type="submit" value='Entrar'>
-		</form>
-	</div>
+  <div id="header">
+    <h1> Sistema de consulta de recibo de pago</h1>
+  </div>
+  <div id="body">
+    <?php if (isset($error)) : ?> 
+      <div id="error">
+        <?php echo $error;?>
+      </div>
+    <?php endif;?>
+    <?php echo form_open('auth/login'); ?>
+      <label>Usuario <input type='text' name='usuario[usuario]'/></label>
+      <label>Password <input type='password' name='usuario[password]'/></label>
+      <input type="submit" value='Entrar'>
+    </form>
+  </div>
 </div>
 </body>
 </html>
