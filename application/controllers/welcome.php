8b91efa49ada23941d2e3944ffa6bf108a268b6a
root <root@debian>
2013-06-24 08:53:44 -0430

    fix welcome

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -17,7 +17,7 @@ class Welcome extends CI_Controller {
 	}
 	private function set_current_user(){
 		$data_user = $this->session->all_userdata();
-		if(isset($data_user['auth'])){
+		if(isset($data_user['auth']))
 			$this -> current_user = User_model::load($data_user);
 		else
 			$this -> current_user = false;
