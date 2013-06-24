c5d4c9d0205c1ca51cdda927f2c8a4a1a64ef2f3
root <root@debian>
2013-06-23 22:13:15 -0430

    rename

--- application/controllers/reportes.php
+++ application/controllers/reportes.php
@@ -1,14 +1,14 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Reporte extends CI_Controller {
+class Reportes extends CI_Controller {
 	private $current_user;
 
 	function __construct() {
 		parent::__construct();
 		$this->load->model('Reporte_model');
 		$this->load->model('User_model');
-//		$current_user = User_model::load($this->session->all_userdata())
-		$this -> current_user = new User_model();
+		$current_user = User_model::load($this->session->all_userdata())
+		$this->set_current_user();
 	}
 	public function index(){
 		$this->load->view('index',array('user' => $this->current_user));
@@ -16,7 +16,19 @@ class Reporte extends CI_Controller {
 	public function reporte(){
 		$this->load->view('index',array('user' => $this->current_user));
 	}
+	public function formulario(){
+		$reporte = new Reporte_model($this->current_user);
+		print_r($reporte->generarComprobante());
+	}
 
+	private function set_current_user(){
+		$this->session->sess_destroy();
+		$data_user = $this->session->userdata('user');
+		if($data_user)
+			$this -> current_user = User_model::load($data_user);
+		else
+			redirect("/");
+	}
 }
 
 /* End of file welcome.php */
